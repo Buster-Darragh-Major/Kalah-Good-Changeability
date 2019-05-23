17c44c9b6e0a4e7fed3cd60f07912b34822a83d8
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2019-05-23 14:05:18 +1200

    Clean up main imports

--- src/kalah/Kalah.java
+++ src/kalah/Kalah.java
@@ -2,11 +2,9 @@ package kalah;
 
 import com.qualitascorpus.testsupport.IO;
 import com.qualitascorpus.testsupport.MockIO;
-import kalah.Bootstrap.*;
-import kalah.Contracts.IO.InputInterpreter;
-import kalah.Contracts.IO.Rendering.OutputFormatter;
+import kalah.Bootstrap.DefaultBoardFactory;
+import kalah.Bootstrap.DefaultKalahRunnerFactory;
 import kalah.Contracts.Model.Board;
-import kalah.Contracts.Rules.KalahRules;
 
 /**
  * This class is the starting point for a Kalah implementation using
@@ -15,9 +13,6 @@ import kalah.Contracts.Rules.KalahRules;
 public class Kalah {
 
 	private Board _board;
-	private OutputFormatter _outputFormatter;
-	private KalahRules _kalahRules;
-	private InputInterpreter _inputInterpreter;
 	private KalahRunner _kalahRunner;
 
 	public static void main(String[] args) {
